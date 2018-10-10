c8a362ab777285ddd7b4100d5c65519f0e33d037
rajcanna <rajacn.net@gmail.com>
2018-10-10 06:36:31 -0400

    annded second class

---
+++ consoleApp/secondclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleApp
+{
+    class secondclass
+    {
+        // adding a comment...
+        // forgot to add this comment.
+    }
+}
